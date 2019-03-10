46d5527d49a6fa4358723b7f01a3725b701f1d2f
christianpiguavebravo <christianpiguavebravo@10.10.1.4>
2019-03-10 23:13:12 +0100

    Modifiche alle classi PrenotazioneAula e PrenotazioneProiettore.

--- progetto-gestioneorari/src/main/java/gestione/orari/entita/PrenotazioneAula.java
+++ progetto-gestioneorari/src/main/java/gestione/orari/entita/PrenotazioneAula.java
@@ -1,5 +1,64 @@
 package gestione.orari.entita;
 
-public class PrenotazioneAula {
+import java.time.LocalDateTime;
 
+public class PrenotazioneAula {
+ 
+	private int idPrenotazioneAula, idAula,idProfessore;
+	private LocalDateTime giornoOraInizio, giornoOraFine;
+	
+	public PrenotazioneAula(int idPrenotazioneAula, int idAula, int idProfessore, LocalDateTime giornoOraInizio,
+			LocalDateTime giornoOraFine) {
+		super();
+		this.idPrenotazioneAula = idPrenotazioneAula;
+		this.idAula = idAula;
+		this.idProfessore = idProfessore;
+		this.giornoOraInizio = giornoOraInizio;
+		this.giornoOraFine = giornoOraFine;
+	}
+
+	public int getIdPrenotazioneAula() {
+		return idPrenotazioneAula;
+	}
+
+	public void setIdPrenotazioneAula(int idPrenotazioneAula) {
+		this.idPrenotazioneAula = idPrenotazioneAula;
+	}
+
+	public int getIdAula() {
+		return idAula;
+	}
+
+	public void setIdAula(int idAula) {
+		this.idAula = idAula;
+	}
+
+	public int getIdProfessore() {
+		return idProfessore;
+	}
+
+	public void setIdProfessore(int idProfessore) {
+		this.idProfessore = idProfessore;
+	}
+
+	public LocalDateTime getGiornoOraInizio() {
+		return giornoOraInizio;
+	}
+
+	public void setGiornoOraInizio(LocalDateTime giornoOraInizio) {
+		this.giornoOraInizio = giornoOraInizio;
+	}
+
+	public LocalDateTime getGiornoOraFine() {
+		return giornoOraFine;
+	}
+
+	public void setGiornoOraFine(LocalDateTime giornoOraFine) {
+		this.giornoOraFine = giornoOraFine;
+	}
+
+
+	
+	
+	
 }
