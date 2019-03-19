6416a81023b70d43e23e944119e86007317ffc82
christianpiguavebravo <christianpiguavebravo@192.168.2.1>
2019-03-19 21:25:05 +0100

    Modifica della classe Autenticazione

--- progetto-gestioneorari/src/main/java/gestione/orari/entita/Autenticazione.java
+++ progetto-gestioneorari/src/main/java/gestione/orari/entita/Autenticazione.java
@@ -8,4 +8,35 @@ private int idPersona;
 
 
 
+public Autenticazione(String username, String password, int idPersona) {
+	super();
+	this.username = username;
+	this.password = password;
+	this.idPersona = idPersona;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public int getIdPersona() {
+	return idPersona;
+}
+public void setIdPersona(int idPersona) {
+	this.idPersona = idPersona;
+}
+@Override
+public String toString() {
+	return "Autenticazione [username=" + username + ", password=" + password + ", idPersona=" + idPersona + "]";
+}
+
+
+
 }
